adc309fd565bd1b9227d5df75f0d12860faf1ff1
Marat141 <terest141@gmail.com>
2025-07-01 15:29:23 +0200

    .

---
+++ src/routes/All_Matriculation_Subjects/Operating_system/LinuxOp_20_1_DHCP/+page.svelte
@@ -0,0 +1,201 @@
+<script lang="ts">
+	import Navigation from '$lib/Components/Navigation.svelte';
+
+	interface Heading {
+		id: number;
+		text: string;
+		level: number;
+		parent?: number;
+		path?: string;
+	}
+
+	let headings: Heading[] = $state([
+		{ id: 1, level: 1, text: 'Úvod do OS', path: '/All_Matriculation_Subjects/Operating_system' },
+		{ id: 2, level: 1, text: 'IT3B PDF soubory' },
+		{
+			id: 3,
+			level: 2,
+			text: 'Aplikační protokoly',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Aplikacni-protokoly'
+		},
+		{
+			id: 4,
+			level: 2,
+			text: 'GDPR souhrn',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/GDPR_Souhrn'
+		},
+		{
+			id: 5,
+			level: 2,
+			text: 'GDPR Technická opatření',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/GDPR-Technicka_opatreni'
+		},
+		{
+			id: 6,
+			level: 2,
+			text: 'Operační systémy základní součásti operačního systému',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_All_13_Operacni_system'
+		},
+		{
+			id: 7,
+			level: 2,
+			text: 'Mac OS část 1',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_MAC_1_1'
+		},
+		{
+			id: 8,
+			level: 2,
+			text: 'Mac OS část 1.2',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_MAC_1_2'
+		},
+		{
+			id: 9,
+			level: 2,
+			text: 'Mac OS část 1.4',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_MAC_1_4_Mail'
+		},
+		{
+			id: 10,
+			level: 2,
+			text: 'Mac OS část 1.7',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_MAC_1_7_Terminal'
+		},
+		{
+			id: 11,
+			level: 2,
+			text: 'Operační systémy Windows 6.2 Funkce zabezpečení ve Windows – přehled',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_W_6_2_security_features_funkce_zabezpeceni_cz'
+		},
+		{
+			id: 12,
+			level: 2,
+			text: 'Operační systémy Windows Ovladače zařízení',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_W10_6_1_device_manager'
+		},
+		{
+			id: 13,
+			level: 2,
+			text: 'Operační systémy Win_1',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_1'
+		},
+		{
+			id: 14,
+			level: 2,
+			text: 'Operační systémy Win_2',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_2'
+		},
+		{
+			id: 15,
+			level: 2,
+			text: 'Operační systémy Win_3',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_3'
+		},
+		{
+			id: 16,
+			level: 2,
+			text: 'Operační systémy Win_4_2',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_4_2'
+		},
+		{
+			id: 17,
+			level: 2,
+			text: 'Operační systémy Win_4',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_4'
+		},
+		{
+			id: 18,
+			level: 2,
+			text: 'Operační systémy Win_5_MMC',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_5_MMC'
+		},
+		{
+			id: 19,
+			level: 2,
+			text: 'Operační systémy Win_7_1_sit_1_cz',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_7_1_sit_1_cz'
+		},
+		{
+			id: 20,
+			level: 2,
+			text: 'Operační systémy Win_7_1_sit_3_cz',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_7_1_sit_3_cz'
+		},
+		{
+			id: 21,
+			level: 2,
+			text: 'Operační systémy Win_7_Windows_Server',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_7_Windows_Server'
+		},
+		{
+			id: 22,
+			level: 2,
+			text: 'Operační systémy Win_8_Sprava_aplikaci_1',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_8_Sprava_aplikaci_1'
+		},
+		{
+			id: 23,
+			level: 2,
+			text: 'Operační systémy Win_9_1_Firewall',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_9_1_Firewall'
+		},
+		{
+			id: 24,
+			level: 2,
+			text: 'Souborove systemy 1_Windows',
+			parent: 2,
+			path: '/All_Matriculation_Subjects/Operating_system/Souborove_systemy_1_Windows'
+		},
+                {
+            id: 25,
+            level: 2,
+            text: 'Linux DHCP',
+            parent: 2,
+            path: '/All_Matriculation_Subjects/Operating_system/LinuxOp_20_1_DHCP'
+        },
+        {
+            id: 26,
+            level: 2,
+            text: 'Windows Server DHCP',
+            parent: 2,
+            path:  '/All_Matriculation_Subjects/Operating_system/Operacni_systemy_Win_Server_14_DHCP',       
+        },
+		{
+			id: 27,
+			level: 1,
+			text: 'Opravné zkoušky',
+			path: '/All_Matriculation_Subjects/Operating_system/Opravne-Zkousky-OS'
+		},
+
+        ]);
+</script>
+
+<main>
+    <div class="layout">
+        <Navigation {headings} />
+        <div class="content">
+            <h1>Linux DHCP</h1>
+            
+        </div>
+    </div>
+</main>
